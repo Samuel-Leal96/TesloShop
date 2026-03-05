14b068f5a1d7b0f4e99ec0623f91e0ed674dd7e6
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-03-05 16:25:03 -0600

    Se agrega paginacion local

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -3,12 +3,13 @@ import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { ProductsService } from '@products/services/products.service';
+import { Pagination } from '@shared/components/pagination/pagination';
 
 // import { ProductCard } from "../../components/product-card/product-card";
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './home-page.html',
 })
 export class HomePage {
@@ -19,8 +20,7 @@ export class HomePage {
     params: () => ({}),
     stream: ({ params }) => {
       return this.productsService.getProducts({
-        limit: 5,
-        gender: 'women'
+        limit: 9
       });
     },
   });
